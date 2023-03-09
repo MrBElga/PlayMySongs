65e01d5a833ec47f2233d860cdd5186451350108
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-03-09 10:21:39 -0300

    validação

--- build/web/controllers/indexcontroller.js
+++ build/web/controllers/indexcontroller.js
@@ -11,7 +11,13 @@ function enviaArquivo(e)
    event.preventDefault(); // evita refresh da tela
                 
    const URL_TO_FETCH = 'UpServlet';
-
+   
+   const vNome = document.getElementById("Musica");
+   vNome.value = vNome.value.replaceAll(/[\W_]/g,"");
+   
+   const vArtista = document.getElementById("Artista");
+   vArtista.value = vArtista.value.replaceAll(/[\W_]/g,"");
+   
    var formData = new FormData(document.getElementById("fdados"));
 
    //formData.append('acao', 'confirmar'); opcional, caso queira inserir outra informação
@@ -20,6 +26,7 @@ function enviaArquivo(e)
           }).then((response)=> {
                return response.text();
           }).then((retorno)=> {
+               
                document.getElementById("feedback").innerHTML=retorno;
                document.getElementById("fdados").reset();
           }).catch(function (error) {
