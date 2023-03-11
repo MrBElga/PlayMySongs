688ba56218bb16f61e79103458048858aadfd058
Venator2345 <vitorhugormartz@gmail.com>
2023-03-11 12:27:57 -0300

    busca músicas

--- build/web/controllers/musicascontroller.js
+++ build/web/controllers/musicascontroller.js
@@ -4,6 +4,11 @@ function listarMusicas(e) {
 
   const URL_TO_FETCH = "ListaMusicasServlet";
   const tab = document.getElementById("tabDados");
+  tab.innerHTML = "";
+  
+  const buscaMusica = document.getElementById("buscaMusicas");
+  const buscaArtista = document.getElementById("buscaArtistas");
+  const buscaEstilo = document.getElementById("buscaEstilos");
 
   fetch(URL_TO_FETCH, { method: "post"})
     .then((response) => {
@@ -12,16 +17,27 @@ function listarMusicas(e) {
     .then((retorno) => {
         let arrMusicas = retorno.split("\n");
         
+        
         for(i = 0; i < arrMusicas.length; i++) {
-            let tr = document.createElement("tr");
-            let td = document.createElement("td");
-            let audioc = document.createElement("audio");
-            audioc.setAttribute("type","audio/mp3");
-            audioc.setAttribute("src","musicas/" + arrMusicas[i]);
-            audioc.setAttribute("controls","");
-            td.appendChild(audioc);
-            tr.appendChild(td);
-            tab.appendChild(tr); 
+            if(arrMusicas[i] != "") {
+                let tr = document.createElement("tr");
+                let td = document.createElement("td");
+                let audioc = document.createElement("audio");
+                let p = document.createElement("h4");
+                const info = arrMusicas[i].split("_");
+                if(info[0].includes(buscaMusica.value)  && (info[1]===(buscaEstilo.value) || buscaEstilo.value === "") && info[2].includes(buscaArtista.value)) {
+                    p.innerHTML = "Título: " + info[0] + "<br>Estilo: " + info[1] + "<br>Artista: " + info[2];
+                    audioc.setAttribute("type","audio/mp3");
+                    audioc.setAttribute("src","musicas/" + arrMusicas[i]);
+                    audioc.setAttribute("controls","");
+                    td.appendChild(p);
+                    td.appendChild(audioc); 
+                    tr.appendChild(td);
+                    tab.appendChild(tr);     
+                }
+                   
+            }
+            
         }
         
     })
